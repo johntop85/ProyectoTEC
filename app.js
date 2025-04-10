a76b7c69bca140cb0131659e653c75421d108c2a
johntop85 <jjaviacion@gmail.com>
2025-04-09 20:40:39 -0500

    Cambio en el menu

---
+++ app.js
@@ -0,0 +1,32 @@
+/* console.log("Mensaje 1");
+
+function sum(a,b){
+    return a + b;
+}
+
+console.log("La suma es: ", sum(2,3));
+
+function mostrarMensaje(){
+    alert('¡Bienvenido a JavaScrip!');
+} */
+
+let x = 10;
+let y = 3.14;
+let nombre = "John T";
+let esActivo = true;
+
+if (x > y) {
+    console.log("El número mayor es x", x)
+} else {
+    console.log("El número mayor es y", y)
+}
+//Ciclos
+x = 1;
+while(x <= 10){
+    console.log("Soy el número (X) ", x)
+    x++;
+}
+
+for (y = 1; y <= 10; y++){
+    console.log("Soy el número (Y) ",y) 
+}
